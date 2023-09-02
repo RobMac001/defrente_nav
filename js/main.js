de3ff16c603d220f59e5e0136a3f8e64098d83e1
LuckyCode-1 <roberto_1318094426@uptecamac.edu.mx>
2023-09-01 22:24:11 -0600

    Primer Commit

---
+++ js/main.js
@@ -0,0 +1,124 @@
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+     
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+
+const themeButton = document.getElementById('theme-button')
+
+
+const sr = ScrollReveal({
+    distance: '30px',
+    duration: 1800,
+    reset: true,
+});
+
+sr.reveal(`.home__data, .home__img, 
+           .decoration__data,
+           .accessory__content,
+           .footer__content`, {
+    origin: 'top',
+    interval: 200,
+})
+
+sr.reveal(`.share__img, .send__content`, {
+    origin: 'left'
+})
+
+sr.reveal(`.share__data, .send__img`, {
+    origin: 'right'
+})
+
+
+
+
+
+
+
+
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+    : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+gsap.from('.home__img-2', 1.2, {opacity: 0, y: 200, delay: .1})
+gsap.from('.home__img-3', 1.2, {opacity: 0, y: 200, delay: .5})
+gsap.from('.home__data', 1.2, {opacity: 0, y: -60, delay: 1})
+gsap.from('.home__bird-1', 1.2, {opacity: 0, x: -80, delay: 1.1})
+gsap.from('.home__bird-2', 1.2, {opacity: 0, x: 80, delay: 1.2})
+gsap.from('.home__img-1', 1.2, {opacity: 0, y: 200, delay: 1.2})
+gsap.from('.home__img-4', 1.2, {opacity: 0, x: 200, delay: 1.3})
